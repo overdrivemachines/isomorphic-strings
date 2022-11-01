8735de36670e616dedc03c4191cc1ee388190d86
get.dipen@gmail.com <get.dipen@gmail.com>
2022-10-31 18:32:30 -0700

    checking accurately if the 2 strings are isomorphic

--- isomorphic-strings.cpp
+++ isomorphic-strings.cpp
@@ -6,15 +6,17 @@ using namespace std;
 bool isIsomorphic(string s, string t);
 
 int main(int argc, char const *argv[]) {
-  string s1 = "egg", t1 = "add";
-  string s2 = "foo", t2 = "bar";
-  string s3 = "paper", t3 = "title";
-  string s4 = "badc", t4 = "baba";
+  string s1 = "egg", t1 = "add";      // true
+  string s2 = "foo", t2 = "bar";      // false
+  string s3 = "paper", t3 = "title";  // true
+  string s4 = "badc", t4 = "baba";    // false
+  string s5 = "paper", t5 = "title";  // true
 
   cout << isIsomorphic(s1, t1) << endl;
   cout << isIsomorphic(s2, t2) << endl;
   cout << isIsomorphic(s3, t3) << endl;
   cout << isIsomorphic(s4, t4) << endl;
+  cout << isIsomorphic(s5, t5) << endl;  // expected: true
 
   return 0;
 }
@@ -27,37 +29,41 @@ bool isIsomorphic(string s, string t) {
     return false;
 
   map<char, char> letters;
+  map<char, char>::iterator it;
 
   cout << "length of string: " << s.length() << endl;
 
-  for (int i = 0; i < s.length(); i++) {
-    cout << s[i] << ":" << t[i];
+  char s_letter, t_letter;
 
-    if (s[i] != t[i]) {
-      cout << " unequal";
-      // check if charecter is in the map
-      if (letters.count(s.at(i))) {
-        // character was found in map
-        cout << " found in map as " << letters[s.at(i)];
-        if (letters[s.at(i)] == t.at(i)) {
-          cout << " OK";
-        } else {
-          cout << " ERROR\n";
+  bool keyFound = false, valueFound = false;
+
+  for (int i = 0; i < s.length(); i++) {
+    s_letter = s.at(i);
+    t_letter = t.at(i);
+
+    cout << s_letter << ":" << t_letter << endl;
+
+    // Check if s_letter is already a key in the map
+    if (letters.count(s_letter)) {
+      // verify t_letter is the value. If not the words are not isomorphic
+      if (letters[s_letter] != t_letter)
+        return false;
+    } else {
+      // s_letter was not found in the map as a key
+
+      // Check if t_letter is already a value in the map
+      // if it is, the word is not isomorphic because
+      // two different s_letters cannot map to the same t_letter
+      for (it = letters.begin(); it != letters.end(); it++) {
+        if (it->second == t_letter) {
           return false;
         }
-      } else {
-        // charecter was not found in the map
-        // so we are going to add it to the map
-        letters[s.at(i)] = t.at(i);
-        cout << " OK";
       }
-    } else {
-      // add the character to the map
-      letters[s.at(i)] = t.at(i);
-      cout << " OK";
-    }
 
-    cout << endl;
+      // t_letter was not found as a value in the map
+      // add s_letter to the map with value t_letter
+      letters.insert(std::pair<char, char>(s_letter, t_letter));
+    }
   }
 
   // cout << endl;
